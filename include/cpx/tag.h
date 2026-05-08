c76bf7116aafe4322f070c9cea131c69a84871ea
aufam <aufa@comtrue.com>
2026-05-08 16:49:14 +0700

    add tag reference

--- include/cpx/tag.h
+++ include/cpx/tag.h
@@ -103,6 +103,88 @@ namespace cpx {
     };
 
     template <typename T>
+    class Tag<T &> {
+    protected:
+        const char *tag;
+        T          *value = nullptr;
+
+    public:
+        using type = T &;
+
+        constexpr Tag(const char *tag, T &value)
+            : tag(tag)
+            , value(std::addressof(value)) {}
+
+        Tag(const char *tag, const T &) = delete;
+        Tag(const char *tag, T &&)      = delete;
+
+        void operator=(const char *) = delete;
+
+        constexpr const T &operator()() const {
+            return *value;
+        }
+
+        constexpr T &operator()() {
+            return *value;
+        }
+
+        constexpr const T &get_value() const {
+            return *value;
+        }
+
+        constexpr T &get_value() {
+            return *value;
+        }
+
+        constexpr std::string_view get_tag(std::string_view key) const {
+            const std::string_view tag = this->tag;
+
+            for (size_t pos = 0;;) {
+                // skip whitespace if any
+                while (pos < tag.size() && tag[pos] == ' ')
+                    pos++;
+                if (pos >= tag.size())
+                    break;
+
+                // find next ":`"
+                const size_t sep = tag.find(":`", pos);
+                if (sep == std::string_view::npos)
+                    break;
+
+                // left part before ":`"
+                std::string_view keys = tag.substr(pos, sep - pos);
+
+                // right part until next backtick
+                size_t end = tag.find('`', sep + 2);
+                if (end == std::string_view::npos)
+                    break;
+
+                std::string_view value = tag.substr(sep + 2, end - (sep + 2));
+
+                for (size_t start = 0; start < keys.size();) {
+                    size_t comma = keys.find(',', start);
+
+                    std::string_view one = keys.substr(start, (comma == std::string_view::npos ? keys.size() : comma) - start);
+
+                    if (one == key)
+                        return value;
+
+                    if (comma == std::string_view::npos)
+                        break;
+
+                    start = comma + 1;
+                }
+
+                // advance past this key–value pair
+                pos = end + 1;
+            }
+
+            return {};
+        }
+    };
+
+
+    template <typename T>
     struct is_tagged : std::false_type {};
 
     template <typename T>
