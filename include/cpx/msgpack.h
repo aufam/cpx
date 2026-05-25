6e1b2861a091e91d93eb9a69d3172860c43244c1
aufam <aufa@comtrue.com>
2026-05-25 15:28:53 +0700

    add msgpack

--- include/cpx/msgpack.h
+++ include/cpx/msgpack.h
@@ -47,8 +47,14 @@ namespace cpx::msgpack {
     template <typename OS, typename From>
     using Serialize = cpx::serde::Serialize<::msgpack::packer<OS>, From>;
 
+    template <typename OS, typename To>
+    using Deserialize = cpx::serde::Serialize<::msgpack::object, To>;
+
     template <typename To>
     using Dump = cpx::serde::Dump<::msgpack::packer<To>, To>;
+
+    template <typename From>
+    using Parse = cpx::serde::Parse<::msgpack::unpacker, From>;
 
     template <typename T>
     [[nodiscard]]
@@ -56,8 +62,21 @@ namespace cpx::msgpack {
 
     template <typename OS, typename T>
     void dump(OS &os, const T &val);
+
+    template <typename T>
+    void parse(const std::string &buffer, T &val);
+
+    template <typename T>
+    std::enable_if_t<std::is_default_constructible_v<T>, T> parse(const std::string &buffer);
+
+    template <typename T>
+    void parse(std::istream &, T &val);
+
+    template <typename T>
+    std::enable_if_t<std::is_default_constructible_v<T>, T> parse(std::istream &);
 } // namespace cpx::msgpack
 
+// primitive types
 template <typename OS, typename T>
 struct cpx::serde::Serialize<::msgpack::packer<OS>, T, std::enable_if_t<std::is_integral_v<T> || std::is_floating_point_v<T>>> {
     ::msgpack::packer<OS> &doc;
@@ -75,6 +94,20 @@ struct cpx::serde::Serialize<::msgpack::packer<OS>, T, std::enable_if_t<std::is_
     }
 };
 
+template <typename T>
+struct cpx::serde::Deserialize<::msgpack::object, T, std::enable_if_t<std::is_integral_v<T> || std::is_floating_point_v<T>>> {
+    const ::msgpack::object &obj;
+
+    void into(T &v) const {
+        try {
+            v = obj.as<T>();
+        } catch (const ::msgpack::type_error &e) {
+            throw type_mismatch_error("primitive", "unknown", e.what());
+        }
+    }
+};
+
+// strings
 template <typename OS, typename CT>
 struct cpx::serde::Serialize<::msgpack::packer<OS>, std::basic_string_view<char, CT>> {
     ::msgpack::packer<OS> &doc;
@@ -87,6 +120,17 @@ struct cpx::serde::Serialize<::msgpack::packer<OS>, std::basic_string_view<char,
     void from_bin(const std::basic_string_view<char, CT> &v) const {
         doc.pack_bin(v.size());
         doc.pack_bin_body(v.data(), v.size());
+    }
+};
+
+template <typename CT>
+struct cpx::serde::Deserialize<::msgpack::object, std::basic_string_view<char, CT>> {
+    const ::msgpack::object &obj;
+
+    void into(std::basic_string_view<char, CT> &v) const {
+        if (obj.type != ::msgpack::type::STR)
+            throw type_mismatch_error("str", "unknown");
+        v = std::basic_string_view<char, CT>{obj.via.str.ptr, obj.via.str.size};
     }
 };
 
@@ -103,6 +147,18 @@ struct cpx::serde::Serialize<::msgpack::packer<OS>, std::basic_string<char, CT, 
     }
 };
 
+template <typename CT, typename A>
+struct cpx::serde::Deserialize<::msgpack::object, std::basic_string<char, CT, A>> {
+    const ::msgpack::object &obj;
+
+    void into(std::basic_string<char, CT, A> &v) const {
+        if (obj.type != ::msgpack::type::STR)
+            throw type_mismatch_error("str", "unknown");
+        v = std::basic_string<char, CT, A>{obj.via.str.ptr, obj.via.str.size};
+    }
+};
+
+// bins
 template <typename OS, typename A>
 struct cpx::serde::Serialize<::msgpack::packer<OS>, std::vector<uint8_t, A>> {
     ::msgpack::packer<OS> &doc;
@@ -110,6 +166,18 @@ struct cpx::serde::Serialize<::msgpack::packer<OS>, std::vector<uint8_t, A>> {
     void from(const std::vector<uint8_t, A> &v) const {
         doc.pack_bin(v.size());
         doc.pack_bin_body(static_cast<const char *>(v.data()), v.size());
+    }
+};
+
+template <typename A>
+struct cpx::serde::Deserialize<::msgpack::object, std::vector<uint8_t, A>> {
+    const ::msgpack::object &obj;
+
+    void into(std::vector<uint8_t, A> &v) const {
+        if (obj.type != ::msgpack::type::BIN)
+            throw type_mismatch_error("bin", "unknown");
+        auto begin = reinterpret_cast<const uint8_t *>(obj.via.bin.ptr);
+        v          = std::vector<uint8_t, A>{begin, begin + obj.via.bin.size};
     }
 };
 
@@ -123,6 +191,20 @@ struct cpx::serde::Serialize<::msgpack::packer<OS>, std::array<uint8_t, N>> {
     }
 };
 
+template <size_t N>
+struct cpx::serde::Deserialize<::msgpack::object, std::array<uint8_t, N>> {
+    const ::msgpack::object &obj;
+
+    void into(std::array<uint8_t, N> &v) const {
+        if (obj.type != ::msgpack::type::BIN)
+            throw type_mismatch_error("bin", "unknown");
+        if (N != obj.via.bin.size)
+            throw size_mismatch_error(N, obj.via.bin.size);
+        ::memcpy(v.data(), obj.via.bin.ptr, N);
+    }
+};
+
+// optional
 template <typename OS, typename T>
 struct cpx::serde::Serialize<
     ::msgpack::packer<OS>,
@@ -138,6 +220,25 @@ struct cpx::serde::Serialize<
     }
 };
 
+template <typename T>
+struct cpx::serde::Deserialize<
+    ::msgpack::object,
+    std::optional<T>,
+    std::enable_if_t<cpx::serde::is_deserializable_v<::msgpack::object, T> && std::is_default_constructible_v<T>>> {
+    const ::msgpack::object &obj;
+
+    void into(std::optional<T> &v) const {
+        if (obj.is_nil())
+            v = std::nullopt;
+        else {
+            if (!v.has_value())
+                v.emplace();
+            Deserialize<::msgpack::object, T>{obj}.into(*v);
+        }
+    }
+};
+
+// variant
 template <typename OS, typename... T>
 struct cpx::serde::Serialize<
     ::msgpack::packer<OS>,
@@ -152,8 +253,44 @@ struct cpx::serde::Serialize<
     }
 };
 
+template <typename... T>
+struct cpx::serde::Deserialize<
+    ::msgpack::object,
+    std::variant<T...>,
+    std::enable_if_t<((cpx::serde::is_deserializable_v<::msgpack::object, T> && std::is_default_constructible_v<T>) && ...)>> {
+    const ::msgpack::object &obj;
+
+    void into(std::variant<T...> &v) const {
+        bool        done = false;
+        std::string type_names;
+        (
+            [&]() {
+                try {
+                    if (!done) {
+                        auto element = T{};
+                        Deserialize<::msgpack::object, T>{obj}.into(element);
+                        v    = std::move(element);
+                        done = true;
+                    }
+                } catch (type_mismatch_error &e) {
+                    type_names += e.expected_type + '|';
+                }
+            }(),
+            ...
+        );
+        if (!done) {
+            type_names.pop_back();
+            throw type_mismatch_error(type_names, "unknown");
+        }
+    }
+};
+
+// array
 template <typename OS, typename T, size_t N>
-struct cpx::serde::Serialize<::msgpack::packer<OS>, std::array<T, N>, std::enable_if_t<!std::is_same_v<T, uint8_t>>> {
+struct cpx::serde::Serialize<
+    ::msgpack::packer<OS>,
+    std::array<T, N>,
+    std::enable_if_t<!std::is_same_v<T, uint8_t> && cpx::serde::is_serializable_v<::msgpack::packer<OS>, T>>> {
     ::msgpack::packer<OS> &doc;
 
     void from(const std::array<T, N> &v) const {
@@ -163,8 +300,35 @@ struct cpx::serde::Serialize<::msgpack::packer<OS>, std::array<T, N>, std::enabl
     }
 };
 
+template <typename T, size_t N>
+struct cpx::serde::Deserialize<
+    ::msgpack::object,
+    std::array<T, N>,
+    std::enable_if_t<!std::is_same_v<T, uint8_t> && cpx::serde::is_deserializable_v<::msgpack::object, T>>> {
+    const ::msgpack::object &obj;
+
+    void into(std::array<T, N> &v) const {
+        if (obj.type != ::msgpack::type::ARRAY)
+            throw type_mismatch_error("array", "unknown");
+        if (obj.via.array.size != N)
+            throw size_mismatch_error(N, obj.via.array.size);
+
+        for (size_t i = 0; i < obj.via.array.size; ++i) {
+            try {
+                Deserialize<::msgpack::object, T>{obj.via.array.ptr[i]}.into(v[i]);
+            } catch (error &e) {
+                e.add_context(i);
+                throw;
+            }
+        }
+    }
+};
+
 template <typename OS, typename T, typename A>
-struct cpx::serde::Serialize<::msgpack::packer<OS>, std::vector<T, A>, std::enable_if_t<!std::is_same_v<T, uint8_t>>> {
+struct cpx::serde::Serialize<
+    ::msgpack::packer<OS>,
+    std::vector<T, A>,
+    std::enable_if_t<!std::is_same_v<T, uint8_t> && cpx::serde::is_serializable_v<::msgpack::packer<OS>, T>>> {
     ::msgpack::packer<OS> &doc;
 
     void from(const std::vector<T, A> &v) const {
@@ -174,12 +338,35 @@ struct cpx::serde::Serialize<::msgpack::packer<OS>, std::vector<T, A>, std::enab
     }
 };
 
+template <typename T, typename A>
+struct cpx::serde::Deserialize<
+    ::msgpack::object,
+    std::vector<T, A>,
+    std::enable_if_t<!std::is_same_v<T, uint8_t> && cpx::serde::is_deserializable_v<::msgpack::object, T>>> {
+    const ::msgpack::object &obj;
+
+    void into(std::vector<T, A> &v) const {
+        if (obj.type != ::msgpack::type::ARRAY)
+            throw type_mismatch_error("array", "unknown");
+
+        v.resize(obj.via.array.size);
+        for (size_t i = 0; i < obj.via.array.size; ++i) {
+            try {
+                Deserialize<::msgpack::object, T>{obj.via.array.ptr[i]}.into(v[i]);
+            } catch (error &e) {
+                e.add_context(i);
+                throw;
+            }
+        }
+    }
+};
+
 template <typename OS, typename K, typename T, typename H, typename P, typename A>
 struct cpx::serde::Serialize<
     ::msgpack::packer<OS>,
     std::unordered_map<K, T, H, P, A>,
     std::enable_if_t<
-        cpx::serde::is_serializable_v<::msgpack::packer<OS>, K> || cpx::serde::is_serializable_v<::msgpack::packer<OS>, T>>> {
+        cpx::serde::is_serializable_v<::msgpack::packer<OS>, K> && cpx::serde::is_serializable_v<::msgpack::packer<OS>, T>>> {
     ::msgpack::packer<OS> &doc;
 
     void from(const std::unordered_map<K, T, H, P, A> &v) const {
@@ -187,6 +374,34 @@ struct cpx::serde::Serialize<
         for (auto &[k, v] : v) {
             Serialize<::msgpack::packer<OS>, K>{doc}.from(k);
             Serialize<::msgpack::packer<OS>, T>{doc}.from(v);
+        }
+    }
+};
+
+template <typename K, typename T, typename H, typename P, typename A>
+struct cpx::serde::Deserialize<
+    ::msgpack::object,
+    std::unordered_map<K, T, H, P, A>,
+    std::enable_if_t<
+        cpx::serde::is_deserializable_v<::msgpack::object, K> && cpx::serde::is_deserializable_v<::msgpack::object, T> &&
+        std::is_default_constructible_v<K> && std::is_default_constructible_v<T>>> {
+    const ::msgpack::object &obj;
+
+    void into(std::unordered_map<K, T, H, P, A> &v) const {
+        if (obj.type != ::msgpack::type::MAP)
+            throw type_mismatch_error("map", "unknown");
+
+        for (size_t i = 0; i < obj.via.map.size; ++i) {
+            K key = {};
+            T val = {};
+            try {
+                Deserialize<::msgpack::object, K>{obj.via.map.ptr[i].key}.into(key);
+                Deserialize<::msgpack::object, T>{obj.via.map.ptr[i].val}.into(val);
+            } catch (error &e) {
+                e.add_context(i);
+                throw;
+            }
+            v.emplace(std::move(key), std::move(val));
         }
     }
 };
@@ -249,12 +464,161 @@ struct cpx::serde::Serialize<::msgpack::packer<OS>, std::tuple<Ts...>> {
     }
 };
 
+template <typename... Ts>
+struct cpx::serde::Deserialize<::msgpack::object, std::tuple<Ts...>> {
+    const ::msgpack::object &obj;
+
+    void into(std::tuple<Ts...> &tpl) const {
+        auto flattened        = cpx::flatten(tpl);
+        using Tpl             = decltype(flattened);
+        constexpr bool is_map = cpx::detail::tuple_has_any_tagged_type_v<Tpl>;
+
+        if (is_map && obj.type != ::msgpack::type::MAP)
+            throw type_mismatch_error("map", "unknown");
+        if (!is_map && obj.type != ::msgpack::type::ARRAY)
+            throw type_mismatch_error("array", "unknown");
+
+        size_t idx = 0;
+        tuple_for_each(flattened, [&](auto &item, const size_t) {
+            const cpx::TagInfo &t         = cpx::msgpack::get_tag_info(item);
+            auto               &v         = cpx::detail::get_underlying_value(item);
+            using T                       = std::decay_t<decltype(v)>;
+            constexpr bool deserializable = cpx::serde::is_deserializable_v<::msgpack::object, T>;
+
+            if constexpr (!deserializable)
+                return;
+
+            const size_t i = idx++;
+            try {
+                if constexpr (is_map) {
+                    if (obj.via.map.ptr[i].key.type == ::msgpack::type::POSITIVE_INTEGER) {
+                        auto got = obj.via.map.ptr[i].key.as<int>();
+                        if (t.field_number != got) {
+                            throw type_mismatch_error(std::to_string(t.field_number), std::to_string(got), "on map key");
+                        }
+                    } else if (obj.via.map.ptr[i].key.type == ::msgpack::type::STR) {
+                        auto &key = obj.via.map.ptr[i].key.via.str;
+                        auto  got = std::string_view(key.ptr, key.size);
+                        if (t.key != got) {
+                            throw type_mismatch_error(std::string(t.key), std::string(got), "on map key");
+                        }
+                    } else {
+                        throw type_mismatch_error("int|str", "unknown");
+                    }
+                    Deserialize<::msgpack::object, T>{obj.via.map.ptr[i].val}.into(v);
+                } else {
+                    Deserialize<::msgpack::object, T>{obj.via.array.ptr[i]}.into(v);
+                }
+            } catch (error &e) {
+                e.add_context(i);
+                throw;
+            }
+        });
+    }
+};
+
 template <typename OS, typename T>
 struct cpx::serde::Serialize<::msgpack::packer<OS>, T, std::enable_if_t<cpx::msgpack::has_reflect_v<T>>> {
     ::msgpack::packer<OS> &doc;
 
     void from(const T &v) const {
-        Serialize<::msgpack::packer<OS>, cpx::msgpack::const_reflect_t<T>>{}.from(cpx::msgpack::reflect_of(v));
+        Serialize<::msgpack::packer<OS>, cpx::msgpack::const_reflect_t<T>>{doc}.from(cpx::msgpack::reflect_of(v));
+    }
+};
+
+template <typename T>
+struct cpx::serde::Deserialize<::msgpack::object, T, std::enable_if_t<cpx::msgpack::has_reflect_v<T>>> {
+    const ::msgpack::object &obj;
+
+    void into(T &v) const {
+        decltype(auto) r = cpx::msgpack::reflect_of(v);
+        Deserialize<::msgpack::object, cpx::msgpack::const_reflect_t<T>>{obj}.from(r);
+    }
+};
+
+template <typename OS>
+struct cpx::serde::Serialize<::msgpack::packer<OS>, std::tm> {
+    ::msgpack::packer<OS> &doc;
+
+    void from(const std::tm &v) const {
+        std::tm tm  = v;
+        time_t  sec = timegm(&tm);
+        Serialize<::msgpack::packer<OS>, time_t>{doc}.from(sec);
+    }
+};
+
+template <>
+struct cpx::serde::Deserialize<::msgpack::object, std::tm> {
+    const ::msgpack::object &obj;
+
+    void into(std::tm &v) const {
+        time_t sec = {};
+        Deserialize<::msgpack::object, time_t>{obj}.into(sec);
+        v = *std::gmtime(&sec);
+    }
+};
+
+template <typename OS>
+struct cpx::serde::Serialize<::msgpack::packer<OS>, std::timespec> {
+    ::msgpack::packer<OS> &doc;
+
+    void from(const std::timespec &v) const {
+        auto r = std::tie(v.tv_sec, v.tv_nsec);
+        Serialize<::msgpack::packer<OS>, decltype(r)>{doc}.from(r);
+    }
+};
+
+template <>
+struct cpx::serde::Deserialize<::msgpack::object, std::timespec> {
+    const ::msgpack::object &obj;
+
+    void into(std::timespec &v) const {
+        auto r = std::tie(v.tv_sec, v.tv_nsec);
+        Deserialize<::msgpack::object, decltype(r)>{obj}.into(r);
+    }
+};
+
+
+template <>
+struct cpx::serde::Parse<::msgpack::unpacker, std::string> {
+    const std::string &buffer;
+
+    template <typename T>
+    void into(T &v) const {
+        ::msgpack::object_handle oh  = ::msgpack::unpack(buffer.data(), buffer.size());
+        ::msgpack::object        obj = oh.get();
+        Deserialize<::msgpack::object, T>{obj}.into(v);
+    }
+};
+
+template <>
+struct cpx::serde::Parse<::msgpack::unpacker, std::istream> {
+    std::istream &stream;
+
+    template <typename T>
+    std::istream &into(T &v) const {
+        ::msgpack::unpacker      pac;
+        ::msgpack::object_handle oh;
+
+        while (!pac.next(oh)) {
+            pac.reserve_buffer(4096);
+            stream.read(pac.buffer(), 4096);
+            auto n = stream.gcount();
+
+            if (n == 0)
+                throw std::runtime_error("EOF");
+
+            pac.buffer_consumed(n);
+        }
+
+        ::msgpack::object obj = oh.get();
+        Deserialize<::msgpack::object, T>{obj}.into(v);
+        return stream;
+    }
+
+    template <typename T>
+    std::istream &operator>>(T &v) const {
+        return into(v);
     }
 };
 
@@ -288,11 +652,38 @@ void cpx::msgpack::dump(OS &os, const T &val) {
     cpx::msgpack::Dump<OS>{os}.from(val);
 }
 
+template <typename T>
+void cpx::msgpack::parse(const std::string &buffer, T &val) {
+    Parse<std::string>{buffer}.into(val);
+}
+
+template <typename T>
+std::enable_if_t<std::is_default_constructible_v<T>, T> cpx::msgpack::parse(const std::string &buffer) {
+    T val = {};
+    Parse<std::string>{buffer}.into(val);
+    return val;
+}
+
+template <typename T>
+void cpx::msgpack::parse(std::istream &stream, T &val) {
+    Parse<std::istream>{stream}.into(val);
+}
+
+template <typename T>
+std::enable_if_t<std::is_default_constructible_v<T>, T> cpx::msgpack::parse(std::istream &stream) {
+    T val = {};
+    Parse<std::istream>{stream}.into(val);
+    return val;
+}
+
 namespace cpx::msgpack {
     constexpr struct IO {
         template <typename OS>
         friend cpx::msgpack::Dump<OS> operator<<(OS &os, const IO &) {
             return {os};
+        }
+        friend cpx::msgpack::Parse<std::istream> operator>>(std::istream &is, const IO &) {
+            return {is};
         }
     } io{};
 } // namespace cpx::msgpack
