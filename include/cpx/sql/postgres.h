0c58c2609ba91268384a7ff7aebe161730b98eb7
aufam <aufa@comtrue.com>
2026-06-10 11:41:56 +0700

    update postgres

--- include/cpx/sql/postgres.h
+++ include/cpx/sql/postgres.h
@@ -37,6 +37,58 @@ namespace cpx::sql::postgres {
 
     template <typename To, typename Enable = void>
     using Deserialize = ::cpx::serde::Deserialize<PGresult, To, Enable>;
+
+    template <typename T>
+    struct OID;
+
+    template <>
+    struct OID<bool> {
+        static constexpr Oid              value = 16;
+        static constexpr std::string_view name  = "bool";
+    };
+
+    template <>
+    struct OID<int16_t> {
+        static constexpr Oid              value = 21;
+        static constexpr std::string_view name  = "int2";
+    };
+
+    template <>
+    struct OID<int32_t> {
+        static constexpr Oid              value = 23;
+        static constexpr std::string_view name  = "int4";
+    };
+
+    template <>
+    struct OID<int64_t> {
+        static constexpr Oid              value = 20;
+        static constexpr std::string_view name  = "int8";
+    };
+
+    template <>
+    struct OID<float> {
+        static constexpr Oid              value = 700;
+        static constexpr std::string_view name  = "float4";
+    };
+
+    template <>
+    struct OID<double> {
+        static constexpr Oid              value = 701;
+        static constexpr std::string_view name  = "float8";
+    };
+
+    template <>
+    struct OID<std::string_view> {
+        static constexpr Oid              value = 25;
+        static constexpr Oid              bytea = 17;
+        static constexpr std::string_view name  = "text";
+    };
+
+    template <>
+    struct OID<std::timespec> {
+        static constexpr Oid              value = 1184;
+        static constexpr std::string_view name  = "timestamptz";
+    };
 } // namespace cpx::sql::postgres
 
 namespace cpx {
@@ -67,9 +119,9 @@ namespace cpx::sql::postgres {
         friend class Connection;
 
     protected:
-        Rows(PGconn *db, int pq_result_format = 0)
+        Rows(PGconn *db, bool is_binary = true)
             : db(db)
-            , pq_result_format(pq_result_format) {
+            , is_binary(is_binary) {
             try {
                 next();
             } catch (std::runtime_error &e) {
@@ -88,7 +140,7 @@ namespace cpx::sql::postgres {
         Rows(Rows &&other) noexcept
             : db(other.db)
             , res(std::exchange(other.res, nullptr))
-            , pq_result_format(other.pq_result_format) {}
+            , is_binary(other.is_binary) {}
 
         Rows &operator=(Rows &&) noexcept = delete;
 
@@ -130,10 +182,22 @@ namespace cpx::sql::postgres {
 
             Row row = {};
             cpx::tuple_for_each(row, [&](auto &item, size_t i) {
-                const int  row       = 0;
-                const int  col       = int(i);
-                const bool is_binary = (pq_result_format == 1);
-                Deserialize<std::decay_t<decltype(item)>>{res, row, col, is_binary}.into(item);
+                const int  row     = 0;
+                const int  col     = int(i);
+                const bool is_null = PQgetisnull(res, row, col);
+
+                Value val;
+                val.buffer = PQgetvalue(res, row, col);
+                val.length = PQgetlength(res, row, col);
+                val.type   = PQftype(res, col);
+                val.format = PQfformat(res, col);
+                try {
+                    Deserialize<std::decay_t<decltype(item)>>{val, is_null}.into(item);
+                } catch (cpx::serde::error &e) {
+                    std::string ctx = "row=" + std::to_string(row) + ",col=" + std::to_string(col);
+                    e.add_context(ctx);
+                    throw;
+                }
             });
 
             return row;
@@ -145,8 +209,8 @@ namespace cpx::sql::postgres {
 
     protected:
         PGconn   *db;
-        PGresult *res              = nullptr;
-        int       pq_result_format = 0;
+        PGresult *res = nullptr;
+        bool      is_binary;
     };
 
     class Connection : public cpx::sql::Connection {
@@ -166,7 +230,7 @@ namespace cpx::sql::postgres {
         }
 
         template <typename Params, typename Row>
-        Rows<Row> operator()(const Statement<Params, Row> &statement, int pq_result_format = 1) {
+        Rows<Row> operator()(const Statement<Params, Row> &statement, bool is_binary = true) {
             std::vector<cpx::sql::postgres::Value> doc;
             doc.reserve(std::tuple_size_v<Params>);
 
@@ -178,7 +242,7 @@ namespace cpx::sql::postgres {
             std::vector<int>          formats(doc.size());
             for (size_t i = 0; i < doc.size(); ++i) {
                 types[i]   = doc[i].type;
-                values[i]  = doc[i].buffer;
+                values[i]  = doc[i].buffer ? doc[i].buffer : doc[i].small_buffer;
                 lengths[i] = doc[i].length;
                 formats[i] = doc[i].format;
             }
@@ -193,13 +257,16 @@ namespace cpx::sql::postgres {
                 values.data(),
                 lengths.data(),
                 formats.data(),
-                pq_result_format
+                is_binary ? 1 : 0
             );
             if (!res)
-                throw std::runtime_error(PQerrorMessage(db));
+                throw std::runtime_error(std::string("PQsendQueryParams: ") + PQerrorMessage(db));
 
-            PQsetSingleRowMode(db);
-            return {db, pq_result_format};
+            res = PQsetSingleRowMode(db);
+            if (!res)
+                throw std::runtime_error(std::string("PQsetSingleRowMode: ") + PQerrorMessage(db));
+
+            return {db, is_binary};
         }
 
         void begin_transaction() override {
@@ -378,10 +445,8 @@ namespace cpx::sql::postgres::detail {
 #define SERIALIZER_FIELDS std::vector<cpx::sql::postgres::Value> &doc;
 
 #define DESERIALIZER_FIELDS                                                                                                      \
-    PGresult *res;                                                                                                               \
-    int       row;                                                                                                               \
-    int       col;                                                                                                               \
-    bool      is_binary;
+    const cpx::sql::postgres::Value &val;                                                                                        \
+    bool                             is_null;
 
 template <typename T>
 struct SERIALIZE(T, std::enable_if_t<std::is_same_v<T, bool> || (std::is_signed_v<T> && (sizeof(T) > 1))>) {
@@ -390,21 +455,8 @@ struct SERIALIZE(T, std::enable_if_t<std::is_same_v<T, bool> || (std::is_signed_
     void from(T value, Oid type = 0) {
         cpx::sql::postgres::Value val;
 
-        if constexpr (std::is_same_v<T, bool>) {
-            val.type = type == 0 ? 16 : type;
-        } else if constexpr (std::is_same_v<T, int16_t>) {
-            val.type = type == 0 ? 21 : type;
-        } else if constexpr (std::is_same_v<T, int32_t>) {
-            val.type = type == 0 ? 23 : type;
-        } else if constexpr (std::is_same_v<T, int64_t>) {
-            val.type = type == 0 ? 20 : type;
-        } else if constexpr (std::is_same_v<T, float>) {
-            val.type = type == 0 ? 700 : type;
-        } else if constexpr (std::is_same_v<T, double>) {
-            val.type = type == 0 ? 701 : type;
-        }
-
-        val.buffer = val.small_buffer;
+        val.type   = type == 0 ? cpx::sql::postgres::OID<T>::value : type;
+        val.buffer = nullptr;
         val.length = sizeof(T);
         val.format = 1;
         cpx::sql::postgres::detail::into_network(value, val.small_buffer);
@@ -428,8 +480,6 @@ struct SERIALIZE(std::basic_string<char, CT, A>) {
     void from(const std::basic_string<char, CT, A> &value, bool is_blob = false) const {
         SERIALIZE(std::basic_string_view<char, CT>){doc}.from(value, is_blob);
     }
-
-    void from(std::basic_string<char, CT, A> &&value, bool is_blob = false) const = delete;
 };
 
 template <typename A>
@@ -439,8 +489,6 @@ struct SERIALIZE(std::vector<uint8_t, A>) {
     void from(const std::vector<uint8_t, A> &value) const {
         SERIALIZE(std::string_view){doc}.from(std::string_view(reinterpret_cast<const char *>(value.data()), value.size()), true);
     }
-
-    void from(std::vector<uint8_t, A> &&value) const = delete;
 };
 
 template <>
@@ -452,7 +500,7 @@ struct SERIALIZE(std::timespec) {
 
         int64_t pg_micros = (value.tv_sec - POSTGRES_EPOCH_DIFF_SECONDS) * 1'000'000 + value.tv_nsec / 1'000;
 
-        SERIALIZE(int64_t){doc}.from(pg_micros, 1184);
+        SERIALIZE(int64_t){doc}.from(pg_micros, cpx::sql::postgres::OID<std::timespec>::value);
     }
 };
 
@@ -511,32 +559,64 @@ struct DESERIALIZE(T, std::enable_if_t<std::is_same_v<T, bool> || (std::is_signe
     DESERIALIZER_FIELDS
 
     void into(T &value) const {
-        cpx::sql::postgres::detail::from_network(value, PQgetvalue(res, row, col));
-        // if (auto ptr = PQgetvalue(res, row, col); is_binary) {
-        //     cpx::sql::postgres::detail::from_network(value, ptr);
-        // } else {
-        //     std::string str = {ptr, (size_t)PQgetlength(res, row, col)};
-        //     if constexpr (std::is_same_v<T, bool>) {
-        //         value = (str == "t" || str == "true" || str == "1");
-        //     } else if constexpr (std::is_same_v<T, int16_t> || std::is_same_v<T, int32_t>) {
-        //         value = std::stoi(str);
-        //     } else if constexpr (std::is_same_v<T, int64_t>) {
-        //         value = std::stol(str);
-        //     } else if constexpr (std::is_same_v<T, float>) {
-        //         value = std::stof(str);
-        //     } else if constexpr (std::is_same_v<T, double>) {
-        //         value = std::stod(str);
-        //     }
-        // }
-    }
-};
+        using namespace cpx::sql::postgres;
 
-template <typename CT, typename A>
-struct DESERIALIZE(std::basic_string<char, CT, A>) {
-    DESERIALIZER_FIELDS
+        if (is_null)
+            throw type_mismatch_error(std::string(OID<T>::name), "null");
 
-    void into(std::basic_string<char, CT, A> &value) const {
-        value = {PQgetvalue(res, row, col), (size_t)PQgetlength(res, row, col)};
+        if (val.format == 0 || val.type == OID<std::string_view>::value) {
+            std::string str(val.buffer, (size_t)val.length);
+            if constexpr (std::is_same_v<T, bool>) {
+                value = (str == "true" || str == "1");
+            } else if constexpr (std::is_same_v<T, int16_t> || std::is_same_v<T, int32_t>) {
+                value = std::stoi(str);
+            } else if constexpr (std::is_same_v<T, int64_t>) {
+                value = std::stol(str);
+            } else if constexpr (std::is_same_v<T, float>) {
+                value = std::stof(str);
+            } else if constexpr (std::is_same_v<T, double>) {
+                value = std::stod(str);
+            }
+        } else if (val.type == OID<bool>::value) {
+            bool v;
+            detail::from_network(v, val.buffer);
+            value = T(v);
+        } else if (val.type == OID<int16_t>::value) {
+            int16_t v;
+            detail::from_network(v, val.buffer);
+            value = T(v);
+        } else if (val.type == OID<int32_t>::value) {
+            int32_t v;
+            detail::from_network(v, val.buffer);
+            value = T(v);
+        } else if (val.type == OID<int64_t>::value || val.type == OID<std::timespec>::value) {
+            int64_t v;
+            detail::from_network(v, val.buffer);
+            value = T(v);
+        } else if (val.type == OID<float>::value) {
+            float v;
+            detail::from_network(v, val.buffer);
+            value = T(v);
+        } else if (val.type == OID<double>::value) {
+            double v;
+            detail::from_network(v, val.buffer);
+            value = T(v);
+        } else if (val.type == OID<std::string_view>::value) {
+            std::string str(val.buffer, (size_t)val.length);
+            if constexpr (std::is_same_v<T, bool>) {
+                value = (str == "true" || str == "1");
+            } else if constexpr (std::is_same_v<T, int16_t> || std::is_same_v<T, int32_t>) {
+                value = std::stoi(str);
+            } else if constexpr (std::is_same_v<T, int64_t>) {
+                value = std::stol(str);
+            } else if constexpr (std::is_same_v<T, float>) {
+                value = std::stof(str);
+            } else if constexpr (std::is_same_v<T, double>) {
+                value = std::stod(str);
+            }
+        } else {
+            throw type_mismatch_error(std::string(OID<T>::name), "oid=" + std::to_string(val.type));
+        }
     }
 };
 
@@ -544,8 +624,30 @@ template <typename CT>
 struct DESERIALIZE(std::basic_string_view<char, CT>) {
     DESERIALIZER_FIELDS
 
-    void into(std::basic_string_view<char, CT> &value) const {
-        value = {PQgetvalue(res, row, col), (size_t)PQgetlength(res, row, col)};
+    void into(std::basic_string_view<char, CT> &value, bool is_blob = false) const {
+        using namespace cpx::sql::postgres;
+
+        const auto expect      = is_blob ? OID<std::string_view>::bytea : OID<std::string_view>::value;
+        const auto expect_name = is_blob ? "bytea" : OID<std::string_view>::name;
+        if (is_null)
+            throw type_mismatch_error(std::string(expect_name), "null");
+        else if ((!is_blob && val.format == 0) || val.type == expect)
+            value = std::basic_string_view<char, CT>(val.buffer, (size_t)val.length);
+        else if (is_blob && val.format == 0)
+            throw error("TODO: need b64 decoding");
+        else
+            throw type_mismatch_error(std::string(expect_name), "oid=" + std::to_string(val.type));
+    }
+};
+
+template <typename CT, typename A>
+struct DESERIALIZE(std::basic_string<char, CT, A>) {
+    DESERIALIZER_FIELDS
+
+    void into(std::basic_string<char, CT, A> &value, bool is_blob = false) const {
+        std::basic_string_view<char, CT> str;
+        DESERIALIZE(decltype(str)){val, is_null}.into(str, is_blob);
+        value = std::basic_string<char, CT, A>(str);
     }
 };
 
@@ -554,11 +656,44 @@ struct DESERIALIZE(std::vector<uint8_t, A>) {
     DESERIALIZER_FIELDS
 
     void into(std::vector<uint8_t, A> &value) const {
-        if (auto ptr = PQgetvalue(res, row, col); is_binary) {
-            auto end = ptr + PQgetlength(res, row, col);
-            value    = {ptr, end};
+        std::string_view str;
+        DESERIALIZE(decltype(str)){val, is_null}.into(str, true);
+        value = std::vector<uint8_t, A>(str.begin(), str.end());
+    }
+};
+
+template <>
+struct DESERIALIZE(std::timespec) {
+    DESERIALIZER_FIELDS
+
+    void into(std::timespec &value) const {
+        using namespace cpx::sql::postgres;
+        if (is_null) {
+            throw type_mismatch_error(std::string(OID<std::timespec>::name), "null");
+        }
+
+        if (val.format == 0 || val.type == OID<std::string_view>::value) {
+            std::string str(val.buffer, size_t(val.length));
+            value = cpx::ts_from_string(str);
+        } else if (val.type == OID<std::timespec>::value) {
+            int64_t micros;
+            DESERIALIZE(int64_t){val, is_null}.into(micros);
+
+            constexpr int64_t PG_TO_UNIX_SECONDS = 946684800LL; // 1970 -> 2000
+
+            int64_t sec    = micros / 1'000'000;
+            int64_t rem_us = micros % 1'000'000;
+
+            // fix negative modulo
+            if (rem_us < 0) {
+                rem_us += 1'000'000;
+                --sec;
+            }
+
+            value.tv_sec  = sec + PG_TO_UNIX_SECONDS;
+            value.tv_nsec = rem_us * 1000;
         } else {
-            throw error("TODO: text format for bytea is not supported yet");
+            throw type_mismatch_error(std::string(OID<std::timespec>::name), "oid=" + std::to_string(val.type));
         }
     }
 };
@@ -568,9 +703,9 @@ struct DESERIALIZE(std::tm) {
     DESERIALIZER_FIELDS
 
     void into(std::tm &value) const {
-        std::time_t t;
-        DESERIALIZE(std::time_t){res, row, col, is_binary}.into(t);
-        value = *std::gmtime(&t);
+        std::timespec t;
+        DESERIALIZE(std::timespec){val, is_null}.into(t);
+        value = *std::gmtime(&t.tv_sec);
     }
 };
 
@@ -579,11 +714,11 @@ struct DESERIALIZE(std::optional<T>, std::enable_if_t<DESERIALIZABLE(T)>) {
     DESERIALIZER_FIELDS
 
     void into(std::optional<T> &value) const {
-        if (PQgetisnull(res, row, col)) {
+        if (is_null) {
             value = std::nullopt;
         } else {
             value.emplace();
-            DESERIALIZE(T){res, row, col, is_binary}.into(*value);
+            DESERIALIZE(T){val, is_null}.into(*value);
         }
     }
 };
